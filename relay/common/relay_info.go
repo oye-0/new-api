98b1e2f40ef80b6ef61815c87f111fbb4c16c03a
oye-0 <oye@l0op.site>
2025-03-14 13:02:35 +0000

    fuck

--- relay/common/relay_info.go
+++ relay/common/relay_info.go
@@ -76,7 +76,7 @@ func GenRelayInfo(c *gin.Context) *RelayInfo {
 	}
 	if info.ChannelType == common.ChannelTypeOpenAI || info.ChannelType == common.ChannelTypeAnthropic ||
 		info.ChannelType == common.ChannelTypeAws || info.ChannelType == common.ChannelTypeGemini ||
-		info.ChannelType == common.ChannelCloudflare {
+		info.ChannelType == common.ChannelCloudflare || info.ChannelType == common.ChannelTypeOllama {
 		info.SupportStreamOptions = true
 	}
 	return info
